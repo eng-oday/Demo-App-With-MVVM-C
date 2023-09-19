d06359f4bba810b7397615bad1c1ba744c442698
eng-oday <odaydieg@gmail.com>
2023-09-19 10:20:20 +0300

    Initial Commit

---
+++ MVVM-C/MVVM_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CApp.swift
+//  MVVM-C
+//
+//  Created by 3rabApp-oday on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
